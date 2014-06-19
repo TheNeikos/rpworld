5ab633f3393962bea2ed20cd9ca36638bcdeb734
chrisjohnson89 <afckazoo@gmail.com>
2014-06-19 15:48:35 -0700

    Added avatar to user model, avatar validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ActiveRecord::Base
   has_many :replies
   has_many :characters
 
+  has_attached_file :avatar
+
   validates :email, :uniqueness => { case_sensitive: false }
   validates :displayname, :uniqueness => { case_sensitive: false },
     :format => { with: /\A[0-9a-zA-Z_-]+\z/ }
+  
+  #Avatar Validations 
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  validates :image, presence: true
 
   def verified?
     self.verified == 1
